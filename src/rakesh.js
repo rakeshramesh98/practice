9b76391207b138de2aa872f5f1de7df753d83462
rakeshramesh98 <rakesh98raji@gmail.com>
2021-01-22 10:20:24 +0530

    init

--- src/rakesh.js
+++ src/rakesh.js
@@ -2,7 +2,217 @@ import React from "react";
 import "./index";
 
 function Rakesh() {
-  return <div className="App">This is a Rakesh page</div>;
+  return (
+      <div>
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+  
+                <div class="container">
+                    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                        
+                            <li class="nav-item" >
+                                <a class="nav-link" href="#">Careers</a>
+                            </li>
+                                <svg width="0.6em" height="0.6em" viewBox="0 0 16 16" class="bi bi-circle-fill ml-1 mt-3 pt-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="8" cy="8" r="8"/>
+                                </svg>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">News</a>
+                            </li>
+                                <svg width="0.6em" height="0.6em" viewBox="0 0 16 16" class="bi bi-circle-fill ml-1 mt-3 pt-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="8" cy="8" r="8"/>
+                                </svg>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Events</a>
+                            </li>
+                                <svg width="0.6em" height="0.6em" viewBox="0 0 16 16" class="bi bi-circle-fill ml-1 mt-3 pt-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="8" cy="8" r="8"/>
+                                </svg>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Investors</a>
+                            </li>
+                                
+                                <li class="nav-item">
+                                <span class="fa fa-globe ml-1 mt-2 pt-1" aria-hidden="true"></span>
+                            </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Worldwide</a>
+                            </li>
+                                <li class="nav-item">
+                                
+                                <span class="fa fa-search ml-1 mt-2 pt-1" aria-hidden="true"></span>
+                            </li>
+                        </ul>
+                    </div>
+                        
+                 </div>  
+            </nav>
+            <section id="showcase1">
+      
+      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+        </ol>
+        
+        <div class="carousel-inner">
+          
+          <div class="carousel-item carousel-image-1 active">
+            <div class="container">
+                <div class="carousel-caption d-none d-sm-block text-left col-md-3 mt-3">
+                    <h1 class="display-3">Heading 1</h1>
+                    <p class="lead">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur at blanditiis atque iste itaque natus aperiam corrupti soluta nobis nemo!</p>
+                    <a href="#" class="btn btn-success btn-lg">See How</a>
+                </div>
+              </div>
+             </div>
+             <div class="carousel-item carousel-image-2">
+              <div class="container">
+                  <div class="carousel-caption d-none d-sm-block text-left col-md-3">
+                      <h1 class="display-3">Heading 2</h1>
+                      <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur at blanditiis atque iste itaque natus aperiam corrupti soluta nobis nemo!</p>
+                      <a href="#" class="btn btn-success bt-lg">See How</a>
+                  </div>
+                </div>
+               </div>
+               <div class="carousel-item carousel-image-3">
+                <div class="container">
+                    <div class="carousel-caption d-none d-sm-block text-left col-md-3">
+                        <h1 class="display-3">Heading 3</h1>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur at blanditiis atque iste itaque natus aperiam corrupti soluta nobis nemo!</p>
+                        <a href="#" class="btn btn-success bt-lg">See How</a>
+                    </div>
+                  </div>
+                 </div>
+                 <div class="carousel-item carousel-image-4">
+                  <div class="container">
+                      <div class="carousel-caption d-none d-sm-block text-left col-md-3">
+                          <h1 class="display-3">Heading 4</h1>
+                          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur at blanditiis atque iste itaque natus aperiam corrupti soluta nobis nemo!</p>
+                          <a href="#" class="btn btn-success bt-lg">See How</a>
+                      </div>
+                    </div>
+                   </div>
+                   <div class="carousel-item carousel-image-5">
+                    <div class="container">
+                        <div class="carousel-caption d-none d-sm-block text-left col-md-3">
+                            <h1 class="display-3">Heading 5</h1>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur at blanditiis atque iste itaque natus aperiam corrupti soluta nobis nemo!</p>
+                            <a href="#" class="btn btn-success bt-lg">See How</a>
+                        </div>
+                      </div>
+                     </div>
+                     <div class="carousel-item carousel-image-6">
+                      <div class="container">
+                          <div class="carousel-caption d-none d-sm-block text-left col-md-3">
+                              <h1 class="display-3">Heading 6</h1>
+                              <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur at blanditiis atque iste itaque natus aperiam corrupti soluta nobis nemo!</p>
+                              <a href="#" class="btn btn-success bt-lg">See How</a>
+                          </div>
+                        </div>
+                       </div>
+        </div>
+        
+        <a class="carousel-control-prev"  href="#carouselExampleCaptions" role="button" data-slide="prev">
+          <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-caret-left" fill="green" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M10 12.796L4.519 8 10 3.204v9.592zm-.659.753l-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+          </svg>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next"  href="#carouselExampleCaptions" role="button" data-slide="next">
+          <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-caret-right" fill="green" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M6 12.796L11.481 8 6 3.204v9.592zm.659.753l5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+          </svg>
+          <span class="sr-only">Next</span>
+        </a>
+        
+      </div>
+  </section>
+    
+
+<footer class="new_footer_area bg_color">
+            <div class="new_footer_top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" >
+                                <h3 class="f-title f_600 t_color f_size_18">Get in Touch</h3>
+                                <p>Don’t miss any updates of our new templates and extensions.!</p>
+                                
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.4s" >
+                                <h3 class="f-title f_600 t_color f_size_18">Download</h3>
+                                <ul class="list-unstyled f_list">
+                                    <li><a href="#">Company</a></li>
+                                    <li><a href="#">Android App</a></li>
+                                    <li><a href="#">ios App</a></li>
+                                    <li><a href="#">Desktop</a></li>
+                                    <li><a href="#">Projects</a></li>
+                                    <li><a href="#">My tasks</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" >
+                                <h3 class="f-title f_600 t_color f_size_18">Help</h3>
+                                <ul class="list-unstyled f_list">
+                                    <li><a href="#">FAQ</a></li>
+                                    <li><a href="#">Term &amp; conditions</a></li>
+                                    <li><a href="#">Reporting</a></li>
+                                    <li><a href="#">Documentation</a></li>
+                                    <li><a href="#">Support Policy</a></li>
+                                    <li><a href="#">Privacy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s">
+                                <h3 class="f-title f_600 t_color f_size_18">Team Solutions</h3>
+                                <div class="f_social_icon">
+                                    <a href="#" class="fa fa-facebook"></a>
+                                    <a href="#" class="fa fa-twitter"></a>
+                                    <a href="#" class="fa fa-linkedin"></a>
+                                    <a href="#" class="fa fa-pinterest"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_bg">
+                    <div class="footer_bg_one"></div>
+                    <div class="footer_bg_two"></div>
+                </div>
+            </div>
+            <div class="footer_bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-sm-7">
+                            <p class="mb-0 f_400">© Rakesh Inc.. 2021 All rights reserved.</p>
+                        </div>
+                        <div class="col-lg-6 col-sm-5 text-right">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    
+      </div>
+  
+        
+  )
 }
 
 export default Rakesh;
